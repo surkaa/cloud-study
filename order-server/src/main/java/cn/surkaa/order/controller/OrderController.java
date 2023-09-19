482359391c9b41dde86072285b606eecb78ba6a8
SurKaa <surkaa67@gmail.com>
2023-09-19 16:06:03 +0800

    update: 在订单中使用

--- order-server/src/main/java/cn/surkaa/order/controller/OrderController.java
+++ order-server/src/main/java/cn/surkaa/order/controller/OrderController.java
@@ -1,5 +1,9 @@
 package cn.surkaa.order.controller;
 
+import cn.surkaa.order.service.OrderService;
+import cn.surkaa.util.vo.OrderWithUserVO;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
 
@@ -9,4 +13,16 @@ import org.springframework.web.bind.annotation.RestController;
 @RestController
 @RequestMapping("/orders")
 public class OrderController {
+
+    private final OrderService orderService;
+
+    public OrderController(OrderService orderService) {
+        this.orderService = orderService;
+    }
+
+    @GetMapping("/{id}")
+    public OrderWithUserVO getOrderWithUser(@PathVariable Long id) {
+        return orderService.getOrderWithUser(id);
+    }
+
 }
