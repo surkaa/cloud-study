f61e2c0c69f02b755ca2051c1402cd5e15cf4f68
SurKaa <surkaa67@gmail.com>
2023-09-19 15:08:36 +0800

    add: user & order 的controller

---
+++ order-server/src/main/java/cn/surkaa/order/controller/OrderController.java
@@ -0,0 +1,12 @@
+package cn.surkaa.order.controller;
+
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+/**
+ * @author SurKaa
+ */
+@RestController
+@RequestMapping("/orders")
+public class OrderController {
+}
---
+++ user-server/src/main/java/cn/surkaa/user/controller/UserController.java
@@ -0,0 +1,13 @@
+package cn.surkaa.user.controller;
+
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+/**
+ * @author SurKaa
+ */
+@RestController
+@RequestMapping("/users")
+public class UserController {
+
+}
