1acf9fb974d4bcd0dfa601ff8cfafb45f07dac60
SurKaa <surkaa67@gmail.com>
2023-09-19 14:23:02 +0800

    update: 将新建的两个module整成springboot module

---
+++ order-server/src/main/java/cn/surkaa/order/OrderServerApplication.java
@@ -0,0 +1,14 @@
+package cn.surkaa.order;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+/**
+ * @author SurKaa
+ */
+@SpringBootApplication
+public class OrderServerApplication {
+    public static void main(String[] args) {
+        SpringApplication.run(OrderServerApplication.class, args);
+    }
+}
---
+++ user-server/src/main/java/cn/surkaa/user/UserServerApplication.java
@@ -0,0 +1,14 @@
+package cn.surkaa.user;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+/**
+ * @author SurKaa
+ */
+@SpringBootApplication
+public class UserServerApplication {
+    public static void main(String[] args) {
+        SpringApplication.run(UserServerApplication.class, args);
+    }
+}
