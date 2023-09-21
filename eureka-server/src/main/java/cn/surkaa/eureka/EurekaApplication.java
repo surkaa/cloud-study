ee0c88dcc186819264be4f7df88d432c00fe03bf
SurKaa <surkaa67@gmail.com>
2023-09-21 12:10:24 +0800

    add: 新module eureka

---
+++ eureka-server/src/main/java/cn/surkaa/eureka/EurekaApplication.java
@@ -0,0 +1,18 @@
+package cn.surkaa.eureka;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.cloud.netflix.eureka.server.EnableEurekaServer;
+
+/**
+ * @author SurKaa
+ */
+@EnableEurekaServer
+@SpringBootApplication
+public class EurekaApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(EurekaApplication.class, args);
+    }
+
+}
