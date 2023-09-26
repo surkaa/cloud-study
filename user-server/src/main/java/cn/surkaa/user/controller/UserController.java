4e10d2929a94a09a4ec1d83af8c3c65eb145cc75
SurKaa <98200894+surkaa@users.noreply.github.com>
2023-09-26 21:31:53 +0800

    update: 添加nacos的配置中心并实现配置的热更新

--- user-server/src/main/java/cn/surkaa/user/controller/UserController.java
+++ user-server/src/main/java/cn/surkaa/user/controller/UserController.java
@@ -1,27 +1,45 @@
 package cn.surkaa.user.controller;
 
 import cn.surkaa.user.service.UserService;
-import cn.surkaa.util.ResultUtil;
-import cn.surkaa.util.vo.BaseVO;
 import cn.surkaa.util.vo.UserVO;
 import lombok.extern.slf4j.Slf4j;
+import org.springframework.beans.factory.annotation.Value;
+import org.springframework.cloud.context.config.annotation.RefreshScope;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
 
+import java.time.LocalDateTime;
+import java.time.format.DateTimeFormatter;
+
 /**
  * @author SurKaa
  */
 @Slf4j
+@RefreshScope // 动态刷新配置文件 感知配置文件的变化
 @RestController
 @RequestMapping("/users")
 public class UserController {
+
+    @Value("${pattern.dateformat}")
+    private String dateformat;
 
     private final UserService userService;
 
     public UserController(UserService userService) {
         this.userService = userService;
+    }
+
+    /**
+     * 测试 获取当前时间 检查配置文件是否生效
+     *
+     * @return 当前时间
+     */
+    @GetMapping("/now")
+    public String now() {
+        return LocalDateTime.now()
+                .format(DateTimeFormatter.ofPattern(dateformat));
     }
 
     // 添加打印方法执行过程(参数 执行时间 返回值...)的注解
