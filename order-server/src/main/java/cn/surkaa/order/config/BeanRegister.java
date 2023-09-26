06b37fdc0f7b292b4288d95dc6088f79027a9b01
SurKaa <98200894+surkaa@users.noreply.github.com>
2023-09-26 19:46:19 +0800

    update: 配置负载均衡
    规则: 随机模式
    加载: 饿汉模式

--- order-server/src/main/java/cn/surkaa/order/config/BeanRegister.java
+++ order-server/src/main/java/cn/surkaa/order/config/BeanRegister.java
@@ -1,5 +1,7 @@
 package cn.surkaa.order.config;
 
+import com.netflix.loadbalancer.IRule;
+import com.netflix.loadbalancer.RandomRule;
 import org.springframework.cloud.client.loadbalancer.LoadBalanced;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
@@ -13,10 +15,25 @@ import org.springframework.web.client.RestTemplate;
 @Configuration
 public class BeanRegister {
 
+    /**
+     * RestTemplate 用于调用其他服务 并且开启负载均衡
+     *
+     * @return RestTemplate
+     */
     @Bean
     @LoadBalanced
     public RestTemplate restTemplate() {
         return new RestTemplate();
+    }
+
+    /**
+     * Ribbon负载均衡策略
+     *
+     * @return RandomRule 随机策略
+     */
+    @Bean
+    public IRule ribbonRule() {
+        return new RandomRule();
     }
 
 }
