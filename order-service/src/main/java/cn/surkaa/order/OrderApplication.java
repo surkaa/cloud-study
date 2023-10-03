f703d0a00cca3522b2d8d7cb27ad3f2df3d4a2ee
SurKaa <98200894+surkaa@users.noreply.github.com>
2023-10-03 18:55:51 +0800

    两个模块的启动类

---
+++ order-service/src/main/java/cn/surkaa/order/OrderApplication.java
@@ -0,0 +1,17 @@
+package cn.surkaa.order;
+
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+/**
+ * @author SurKaa
+ */
+@SpringBootApplication
+public class OrderApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(OrderApplication.class, args);
+    }
+
+}
